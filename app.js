649feab7adcf9dc189786ba08422bcbf302b2ee7
nicopons24 <nicopons24@gmail.com>
2016-05-09 18:12:33 +0200

    POST + GET notas

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 var methodOverride = require('method-override');
 var ninos = require('./controller/ninos.js');
 var courses = require('./controller/cursos.js');
-var calif = require('./controller/calificaciones.js');
+var notas = require('./controller/notas.js');
 var users = require('./controller/usuarios.js');
 var ejercicios = require('./controller/ejercicios.js');
 var bd = require('./bd.js');
@@ -235,8 +235,53 @@ router.post('/curso/ejercicio', function (req, res) {
 
 });
 
-router.get('/ejercicios/:idEjercicio/notas', function(req, res) {
+router.get('/curso/:idCurso/ejercicios/:idEjercicio/notas/:idNino', function (req, res) {
+    var params = req.params;
+    var head = req.headers;
+    users.autenticate(head, function (isOk, data) {
+        if (isOk) notas.getNotasEjercicio(params, function (json, code) {
+            res.json(json);
+            res.statusCode = code;
+        });
+        else {
+            var json = {
+                res: 2,
+                result: data
+            };
+            res.json(json);
+            res.statusCode = 400;
+        }
+    })
+});
 
+router.post('/ejercicio/nota', function (req, res) {
+    var body = req.body;
+    var head = req.headers;
+    users.autenticate(head, function (isOk, data) {
+        if (isOk) {
+            if (data.tipo === userTypes[0] || data.tipo === userTypes[1])
+                notas.setNotasEjercicio(body, function (json, code) {
+                    res.json(json);
+                    res.statusCode = code;
+                });
+            else {
+                var jsonA = {
+                    res: 3,
+                    result: "Permission Denied"
+                };
+                res.json(jsonA);
+                res.statusCode = 400;
+            }
+        }
+        else {
+            var json = {
+                res: 2,
+                result: data
+            };
+            res.json(json);
+            res.statusCode = 400;
+        }
+    })
 });
 
 app.use(router);
---
+++ controller/notas.js
@@ -0,0 +1,110 @@
+/**
+ * Created by buny on 05/05/2016.
+ */
+var bd = require('../bd.js');
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+function getNotasDia(body, callback) {
+    var sql = "select c.id, c.comportamiento ,c.puntualidad ,c.ejercicios ,c.ayudaCompanieros from calificaciones c " +
+        "inner join calificaciones_has_ninos cn on c.id = cn.idCalificacion " +
+        "where cn.fecha = '" + body.fecha + "' and c.idCurso = " + body.idCurso + " and cn.idNino = " + body.idNino + ";";
+    bd.query(sql, function (err, rows, fields) {
+        var json = {};
+        var statusCode = 400;
+        if (err) {
+            json.res = 0;
+            json.result = err
+        } else {
+            statusCode = 200;
+            json.res = 1;
+            json.calificaciones = rows[0] || null;
+        }
+        callback(json, statusCode);
+    });
+}
+
+function getNotasMes(body, callback) {
+    var sql = "select c.id, c.comportamiento, c.puntualidad, c.ejercicios, c.ayudaCompanieros from calificaciones c " +
+        "inner join calificaciones_has_ninos cn on c.id = cn.idCalificacion " +
+        "where month(cn.fecha) = month('" + body.fecha + "') and c.idCurso = " + body.idCurso + " and cn.idNino = " + body.idNino + ";";
+    bd.query(sql, function (err, rows, fields) {
+        var json = {};
+        var statusCode = 400;
+        if (err) {
+            json.res = 0;
+            json.result = err;
+        }
+        else {
+            statusCode = 200;
+            json.res = 1;
+            json.nombreMes = monthNames[body.fecha - 1];
+            json.calificaciones = rows || null;
+        }
+        callback(json, statusCode);
+    });
+}
+
+function getNotasEjercicio(params, callback) {
+    var sql = "select c.* from calificaciones c " +
+        "inner join calificaciones_has_ninos cn on c.id = cn.idCalificacion " +
+        "where c.idEjercicio = " + params.idEjercicio +
+        " and cn.idNino = " + params.idNino;
+    console.log(sql);
+    bd.query(sql, function (err, rows, fields) {
+        var json = {};
+        var statusCode = 400;
+        if (err) {
+            json.res = 0;
+            json.result = err;
+        }
+        else {
+            statusCode = 200;
+            json.res = 1;
+            json.calificaciones = rows[0] || null;
+        }
+        callback(json, statusCode);
+    });
+}
+
+function setNotasEjercicio(body, callback) {
+    var sql = "insert into calificaciones(comportamiento,puntualidad,ejercicios, ayudaCompanieros, idEjercicio) " +
+        "values (" + body.comportamiento + ", " + body.puntualidad + ", " +
+        body.ejercicios + ", " + body.ayuda + ", " + body.idEjercicio + ")";
+    bd.query(sql, function (err, rows, fields) {
+        if (err) {
+            callback({
+                res: 0,
+                result: err
+            }, 400);
+        }
+        else {
+            var sql = "insert into calificaciones_has_ninos(idCalificacion, idNino)" +
+                "values (" + rows.insertId + ", " + body.idNino + ")";
+            bd.query(sql, function (err, rows, fields) {
+                var json = {};
+                var statusCode = 400;
+                if (err) {
+                    json.res = 0;
+                    json.result = err;
+                }
+                else {
+                    statusCode = 200;
+                    json.res = 1;
+                    json.result = {
+                        affectedRows: rows.affectedRows,
+                        insertId: rows.insertId,
+                        message: rows.message
+                    };
+                }
+                callback(json, statusCode);
+            });
+        }
+    });
+}
+module.exports.getNotasDia = getNotasDia;
+module.exports.getNotasMes = getNotasMes;
+module.exports.getNotasEjercicio = getNotasEjercicio;
+module.exports.setNotasEjercicio = setNotasEjercicio;
